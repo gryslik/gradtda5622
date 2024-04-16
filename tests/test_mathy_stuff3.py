62f9ee63c9bfcc12e6a1bc0de2b0df14d9e2eb59
Greg Ryslik <gryslik@gmail.com>
2024-04-16 18:32:19 -0400

    making another simple change.

--- tests/test_mathy_stuff3.py
+++ tests/test_mathy_stuff3.py
@@ -1,6 +1,10 @@
-# def test_divide_positive_numbers_fail() -> None:
-#    """Test that divide returns the correct result when given two integers."""
-#    assert divide(1, 2) == 0.6
+from math_stuff.math_utils import divide
+
+
+def test_divide_positive_numbers_fail() -> None:
+    """Test that divide returns the correct result when given two integers."""
+    assert divide(1, 2) == 0.6
+
 
 def test_divide_success() -> None:
     assert 0 == 0
